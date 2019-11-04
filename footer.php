89b6ccdcee50e3664f944b622dad8a1150f3d3ce
Andrey Kozlov <andreydmitrievich666@gmail.com>
2019-11-04 23:34:17 +0300

    update

--- footer.php
+++ footer.php
@@ -4,47 +4,90 @@
 
     <!-- begin footer -->
     <footer class="l-footer footer">
-        <div class="footer__container">
-
-            <div class="footer__call">
-                <a class="footer__phone" href="tel:+971503749323">+971 50 374 9323</a>
-                <a class="footer__whatsapp" href="https://api.whatsapp.com/send?phone=+971503749323" target="_blank">WhatsApp Chat</a>
-            </div>
-
-            <div class="footer__menu">
-                <?php wp_nav_menu( [
-                    'theme_location'  => 'foot_menu',
-                    'container'       => 'nav', 
-                    'container_class' => 'menu-footer', 
-                    'container_id'    => '',
-                    'menu_class'      => '', 
-                    'menu_id'         => '',
-                    'echo'            => true,
-                    'fallback_cb'     => 'wp_page_menu',
-                    'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
-                    'depth'           => 0,
-                    'walker'          => new True_Walker_Nav_Menu,
-                ] ); ?>
-            </div>
-
-            
-
-            <div class="footer__bottom">
-                <a class="footer__logo" href="/"><img src="assets/images/logo-footer.png" alt="Logo"></a>
-                <p><span>Address:</span> Rent Ferrari Dubai, Ofice 1003, <br>Sobha Ivory II, Business Bay, Dubai, UAE</p>
+        <div class="footer__menu">
+            <div class="menu-footer js-footer-menu">
+                <!-- <nav class="menu-footer__container">
+                    <ul>
+                        <li class="-has-dropdown is-active">
+                            <a href="">Category 1</a>
+                            <div class="menu-footer__dropdown -col-2">
+                                <ul>
+                                    {% for i in 0..12 %}
+                                    <li {% if (loop.index == 3) %}class="is-active"{% endif %}>
+                                        <a href="#">Subcategory {{loop.index}}</a>
+                                    </li>
+                                    {% endfor %}
+                                </ul>
+                            </div>
+                        </li>
+                        {% for i in 0..2 %}
+                        <li class="-has-dropdown">
+                            <a href="">Category {{loop.index + 1}}</a>
+                            <div class="menu-footer__dropdown">
+                                <ul>
+                                    {% for i in 0..7 %}
+                                    <li><a href="#">Subcategory {{loop.index}}</a></li>
+                                    {% endfor %}
+                                </ul>
+                            </div>
+                        </li>
+                        {% endfor %}
+                    </ul>
+                </nav> -->
+                <div class="footer__menu">
+                    <?php wp_nav_menu( [
+                        'theme_location'  => 'foot_menu',
+                        'container'       => 'nav', 
+                        'container_class' => 'menu-footer', 
+                        'container_id'    => '',
+                        'menu_class'      => '', 
+                        'menu_id'         => '',
+                        'echo'            => true,
+                        'fallback_cb'     => 'wp_page_menu',
+                        'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
+                        'depth'           => 0,
+                        'walker'          => new True_Walker_Nav_Menu,
+                    ] ); ?>
+                </div>
             </div>
         </div>
 
-        <!-- powered_by_google -->
-        <div itemscope itemtype="http://schema.org/Place">
-            <div itemtype="http://schema.org/AggregateRating" itemscope="" itemprop="aggregateRating">
-                <meta itemprop="ratingValue" content="4.9"/>
-                <meta itemprop="ratingCount" content="172"/>
-                <meta itemprop="bestRating" content="5"/>
+        <div class="footer__bottom">
+
+            <div class="social-icon">
+                <a href="#" class="icon-vkontakte" rel="noreferrer noopener">vkontakte</a>
+                <a href="#" class="icon-facebook" rel="noreferrer noopener">facebook</a>
+                <a href="#" class="icon-instagram" rel="noreferrer noopener">instagram</a>
+                <a href="#" class="icon-twitter" rel="noreferrer noopener">twitter</a>
+                <a href="#" class="icon-google" rel="noreferrer noopener">google</a>
+                <!-- <a href="#" class="icon-odnoklassniki" rel="noreferrer noopener">odnoklassniki</a>
+                |
+                <a href="#" class="icon-youtube" rel="noreferrer noopener">youtube</a>
+                <a href="#" class="icon-youtube2" rel="noreferrer noopener">youtube</a>
+                <a href="#" class="icon-linkedin" rel="noreferrer noopener">linkedin</a>
+                <a href="#" class="icon-flickr" rel="noreferrer noopener">flickr</a>
+                <a href="#" class="icon-mailru" rel="noreferrer noopener">mailru</a>
+                <a href="#" class="icon-pinterest" rel="noreferrer noopener">pinterest</a>
+                <a href="#" class="icon-tumblr" rel="noreferrer noopener">tumblr</a>
+                |
+                <a href="#" class="icon-whatsapp" rel="noreferrer noopener" target="_blank">whatsapp</a>
+                <a href="#" class="icon-viber" rel="noreferrer noopener" target="_blank>viber</a>
+                <a href="#" class="icon-telegram" rel="noreferrer noopener" target="_blank>telegram</a>
+                <a href="#" class="icon-skype" rel="noreferrer noopener" target="_blank">skype</a> -->
             </div>
-        </div>
         
-    </footer> <!-- end footer -->
+            <div itemscope itemtype="http://schema.org/Place">
+                <div itemtype="http://schema.org/AggregateRating" itemscope="" itemprop="aggregateRating">
+                    <meta itemprop="ratingValue" content="4.9"/>
+                    <meta itemprop="ratingCount" content="172"/>
+                    <meta itemprop="bestRating" content="5"/>
+                </div>
+            </div>
+
+        </div>
+    </footer>
+
+    <!-- end footer -->
 
 <?php wp_footer(); ?>
 
--- header.php
+++ header.php
@@ -12,35 +12,50 @@
 <!-- begin header -->
     <header class="l-header header">
         <div class="header__container">
-            
+
             <?php if ( has_custom_logo() ) : ?>
                 <div class="header__logo"><?php the_custom_logo(); ?></div>
             <?php endif; ?>
 
-            <div class="header__menu">
-                <?php wp_nav_menu( [
-                    'theme_location'  => 'main_menu',
-                    'container'       => 'nav', 
-                    'container_class' => 'main-menu js-menu-smart js-menu-mobile', 
-                    'container_id'    => '',
-                    'menu_class'      => 'main-menu__root is-root', 
-                    'menu_id'         => '',
-                    'echo'            => true,
-                    'fallback_cb'     => 'wp_page_menu',
-                    'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
-                    'depth'           => 0,
-                    'walker'          => new True_Walker_Nav_Menu,
-                ] ); ?>
+            <div class="header__widgets">
+                <div class="header__widgets-item header-search">
+                    <a class="header-search__link" href="">Поиск</a>
+                </div>
+                <div class="header__widgets-item header-profile-widget">
+                    <a class="header-profile-widget__link" href="">Личный кабинет</a>
+                </div>
+                <div class="header__widgets-item header-favorites-widget">
+                    <a class="header-favorites-widget__count" href="">Избранное<span>5</span></a>
+                </div>
+                <div class="header__widgets-item header-cart-widget js-cart-widget">
+                    <a class="header-cart-widget__counter" href="{{ url.orderCart }}">Корзина<span>12</span></a>
+                    <div class="header-cart-widget__dropdown">
+                    </div>
+                </div>
             </div>
         
-            <div class="header__call">
-                <a class="header__whatsapp" href="https://api.whatsapp.com/send?phone=+971503749323" target="_blank">WhatsApp Chat</a>
-                <a class="header__phone" href="tel:+971503749323">+971 50 374 9323</a>
-            </div>
         </div>
 
+        <nav class="header__main-menu">
+            <!-- <button data-menu-mobile--switcher-btn><span>menu</span></button> -->
+            <?php wp_nav_menu( [
+                'theme_location'  => 'main_menu',
+                'container'       => 'nav', 
+                'container_class' => 'main-menu js-menu-smart js-menu-mobile', 
+                'container_id'    => '',
+                'menu_class'      => 'main-menu__root is-root', 
+                'menu_id'         => '',
+                'echo'            => true,
+                'fallback_cb'     => 'wp_page_menu',
+                'items_wrap'      => '<ul class="%2$s">%3$s</ul>',
+                'depth'           => 0,
+                'walker'          => new True_Walker_Nav_Menu,
+            ] ); ?>
+        </nav>
     </header>
     <!-- end header -->
+
+    
 
     <!-- begin main -->
     <main class="l-main">
